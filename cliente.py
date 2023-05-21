6c8b39ac92711a43fee60121c2f26a4ec90a44f9
KimRuedasR <kruedas09@alumnos.uaq.mx>
2023-05-21 16:23:45 -0600

    🗿 feat(resend): add Renvio y verificacion, ya furula esto  🗿

--- cliente.py
+++ cliente.py
@@ -6,18 +6,20 @@ def Main():
     port = 5000
     address = (host, port)
 
-    # Generamos la llave(16 bytes), una sal y el tipo de encriptación
+    # Generamos la llave (16 bytes), una sal y el tipo de encriptación
     llave = b'Esta_es_mi_llave'
     sal = b'Esta_es_mi_-sal-'
-    cifrado = AES.new(llave, AES.MODE_EAX, sal)
 
     # Conexión del socket, utilizando TCP
     cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cliente.connect(address)
 
-    # Abrimos la imagen para lectura
+    # Abrimos la imagen para lectura en modo binario
     with open('imagen_original.jpg', 'rb') as archivo:
         datos = archivo.read()
+
+    # Inicializamos el objeto de cifrado AES
+    cifrado = AES.new(llave, AES.MODE_EAX, sal)
 
     # Encriptamos los datos
     encriptado = cifrado.encrypt(datos)
@@ -30,6 +32,34 @@ def Main():
     print('  Archivo enviado  ')
     print('-----------------------')
 
+    # Recibir los datos de la imagen devuelta por el servidor
+    datos_cliente = b''
+    while True:
+        datos = cliente.recv(1024)
+        if datos:
+            if datos[-5:] == b'<FIN>':
+                datos_cliente += datos[:-5]
+                break
+            else:
+                datos_cliente += datos
+        else:
+            break
+
+    # Inicializamos un nuevo objeto de cifrado AES para descifrar los datos
+    descifrado_cifrado = AES.new(llave, AES.MODE_EAX, sal)
+    descifrado_bytes = descifrado_cifrado.decrypt(datos_cliente)
+
+    # Comparar los datos descifrados con los datos originales de la imagen encriptada
+    if descifrado_bytes == encriptado:
+        print('\n-----------------------')
+        print('  ¡Verificación exitosa!  ')
+        print('-----------------------')
+    else:
+        print('\n-----------------------')
+        print('  ¡Verificación fallida!  ')
+        print('-----------------------')
+
+    # Cerrar el cliente
     cliente.close()
 
 if __name__ == "__main__":
--- servidor.py
+++ servidor.py
@@ -5,14 +5,14 @@ def Main():
     host = "localhost"
     port = 5000
     address = (host, port)
-    buff = 1024
+    buff = 2048
 
-    # Generamos la llave(16 bytes) y tipo de encriptación
+    # Generamos la llave (16 bytes) y tipo de encriptación
     llave = b'Esta_es_mi_llave'
     sal = b'Esta_es_mi_-sal-'
     cifrado = AES.new(llave, AES.MODE_EAX, sal)
 
-    # Establecemos el socket, utilizando TCP
+    # Establecemos el socket utilizando TCP
     servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     servidor.bind(address)
     servidor.listen()
@@ -20,33 +20,52 @@ def Main():
     print('  Esperando archivo...  ')
     print('************************')
 
-    # Acepta la conexion y socket de un cliente
+    # Acepta la conexión y el socket de un cliente
     cliente, cliente_add = servidor.accept()
 
     archivo_nombre = "imagen_nueva.jpg"
 
-    # Abrimos la imagen para escritura y definimos cuando se acaba de transmitir el tamaño de la imagen
-    archivo = open(archivo_nombre, 'wb')
-    terminado = False
-    archivo_bytes = b''
+    # Abrimos el archivo para escritura en modo binario
+    with open(archivo_nombre, 'wb') as archivo:
+        terminado = False
+        archivo_bytes = b''
 
-    # Descargamos los datos de la imagen
-    while not terminado:
-        datos = cliente.recv(buff)
-        # Revisamos los últimos 5 bytes enviados
-        if archivo_bytes[-5:] == b'<FIN>':
-            terminado = True
-        else:
-            archivo_bytes += datos
+        # Descargamos los datos de la imagen
+        while not terminado:
+            datos = cliente.recv(buff)
+            if datos:
+                # Revisamos los últimos 5 bytes enviados
+                if datos[-5:] == b'<FIN>':
+                    terminado = True
+                    archivo_bytes += datos[:-5]
+                else:
+                    archivo_bytes += datos
+            else:
+                break
 
-    archivo.write(cifrado.decrypt(archivo_bytes[:-5]))
+        descifrado_bytes = cifrado.decrypt(archivo_bytes)
+        archivo.write(descifrado_bytes)
+
     print('\n-----------------------')
     print('  ¡Archivo recibido!  ')
     print('-----------------------')
-    print('-- Nombre del archivo:', archivo_nombre,' --')
+    print('-- Nombre del archivo:', archivo_nombre, ' --')
 
-    # Cerramos el archivo y sockets
-    archivo.close()
+    # Leer los datos del archivo recibido en el servidor
+    with open(archivo_nombre, 'rb') as archivo_servidor:
+        datos_servidor = archivo_servidor.read()
+
+    # Enviar los datos de la imagen nuevamente al cliente
+    cliente.sendall(datos_servidor)
+
+    # Esperar una respuesta del cliente para asegurarse de que ha recibido los datos
+    cliente.settimeout(5)
+    try:
+        cliente.recv(1)
+    except socket.timeout:
+        pass
+
+    # Cerrar el cliente y el servidor
     cliente.close()
     servidor.close()
 
